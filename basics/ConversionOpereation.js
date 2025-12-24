03db788487f27eba565dbdf5d2a6664d6aa272f2
Sumitmishra3003 <sumitind272001@gmail.com>
2025-12-24 07:46:39 +0000

    successful

---
+++ basics/ConversionOpereation.js
@@ -0,0 +1,18 @@
+let score = "33"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)  // N should be in capital letters
+
+console.log(typeof valueInNumber) // it convert all type of datatypes into numbrt
+
+//"33" - 33
+//"33abc" - NaN
+// true - 1
+
+let isloggedin = 1
+let booleanloggedin = Boolean(isloggedin)  // B should be in capital
+console.log(booleanloggedin)
+
+// for empty string, boolean gives false
+// for "sumit" bolean gives true
