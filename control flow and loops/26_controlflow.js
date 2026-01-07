17b7f569fcd7c98103e8be0e585e7662ad87d840
Sumitmishra3003 <sumitind272001@gmail.com>
2026-01-07 17:36:30 +0000

    loops

---
+++ control flow and loops/26_controlflow.js
@@ -0,0 +1,20 @@
+// if-else, switch case and other concept are same as CPP
+
+
+// flasy values : false, 0, -0, bigint 0n, "", null, undefined, NaN
+// truthy values : "0", "false", " ", [], {}, function(){}
+// value other then that are truthy values
+
+
+// nullish coalescing operator (??): null undefined : used to check the null and undefined
+// it return the first value other then null and undefined
+let val1
+//val1 = 5 ?? 10 // here it give 5
+val1 = null ?? 10 // here it gives 10
+console.log(val1)
+
+
+// terneary operator (?)
+const price = 100
+price >= 80 ? console.log("less then 80") : console.log("more then 80")
+
---
+++ control flow and loops/27_forloop.js
@@ -0,0 +1,18 @@
+// for loop is same as CPP
+
+// for loop using break and continue
+
+for (let index = 1; index <= 20; index++){
+    if(index == 5){
+        console.log(`5 detected`)
+        break
+    }
+    console.log(`value of i is ${index}`)
+}
+for (let index = 1; index <= 20; index++){
+    if(index == 5){
+        console.log(`5 detected`)
+        continue
+    }
+    console.log(`value of i is ${index}`)
+}
